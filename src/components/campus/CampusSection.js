123a7dbea301fa0a9af08ba768efeff9d41ac8d2
Manas Gautam <gautammanas2000@gmail.com>
2021-11-11 17:04:06 +0530

    campus

--- src/components/campus/CampusSection.js
+++ src/components/campus/CampusSection.js
@@ -34,36 +34,32 @@ const CampusSection = () => {
       <div className='container-xxl px-xxl-0 px-lg-5 px-md-4 px-sm-3'>
         <h1>{websiteContent[0]?.data[22]?.field_data}</h1>
         <div className='partners-images pt-3'>
-          <div className='row'>
-            <div className='col-lg-6 col-md-10 col-12 offset-lg-3 offset-md-1'>
-              <Swiper
-                spaceBetween={0}
-                slidesPerView={1}
-                autoplay={{ autoplay: true, delay: 10000 }}
-                breakpoints={{
-                  // when window width is >= 0px
-                  0: {
-                    slidesPerView: 3,
-                  },
-                  // when window width is >= 1000px
-                  600: {
-                    slidesPerView: 5,
-                  },
-                  900: {
-                    slidesPerView: 7,
-                  },
-                }}
-              >
-                {partner[0]?.data?.map((x, index) => {
-                  return (
-                    <SwiperSlide key={index}>
-                      <Campus url={x.partners_img} />
-                    </SwiperSlide>
-                  );
-                })}
-              </Swiper>
-            </div>
-          </div>
+          <Swiper
+            spaceBetween={0}
+            slidesPerView={1}
+            autoplay={{ autoplay: true, delay: 10000 }}
+            breakpoints={{
+              // when window width is >= 0px
+              0: {
+                slidesPerView: 3,
+              },
+              // when window width is >= 1000px
+              600: {
+                slidesPerView: 5,
+              },
+              900: {
+                slidesPerView: 7,
+              },
+            }}
+          >
+            {partner[0]?.data?.map((x, index) => {
+              return (
+                <SwiperSlide key={index}>
+                  <Campus url={x.partners_img} />
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
         </div>
       </div>
     </div>
